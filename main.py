dc5d7362b953c087dd6987889b0af14fdcb1db23
Lottecnx <taesuphappc@gmail.com>
2025-04-08 06:40:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, ImageSendMessage
 import openai
 import os
 import json
